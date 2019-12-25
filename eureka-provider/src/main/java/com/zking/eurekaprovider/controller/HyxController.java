00f8a1d8fd07a8081f12e7fd401c24e69890a7c8
lenovo <1633781338@qq.com>
2019-12-25 16:47:43 +0800

    1.0

--- eureka-provider/src/main/java/com/zking/eurekaprovider/controller/HyxController.java
+++ eureka-provider/src/main/java/com/zking/eurekaprovider/controller/HyxController.java
@@ -3,22 +3,31 @@ package com.zking.eurekaprovider.controller;
 import com.zking.eurekaprovider.service.IHyxService;
 import com.zking.eurekaprovider.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("hyx")
 public class HyxController {
 
     @Autowired
     private IHyxService iHyxService;
 
+    JsonData jsonData;
+    @ModelAttribute
+    public void init(Model model) {
+//        System.out.println("init...");
+        //用来初始化数据
+        jsonData = new JsonData();
+    }
 
-    @RequestMapping("selectid")
-    public JsonData Hello(String name){
-        String msg="生产者["+"]为您服务,"+"ts:"+System.currentTimeMillis();
-        JsonData jsonData=new JsonData();
+
+    @RequestMapping(value="/selectid",produces = {"application/json;charset=UTF-8"})
+    public JsonData test1() {
         jsonData.setCode(0);
-        jsonData.put("name",name);
+        jsonData.setResult(iHyxService.selectByPrimaryKey(1));
         return jsonData;
     }
 }
